fce2449b6b3e480f69164f4f004cd37d3ac599e4
harrison <hwhite9@wisc.edu>
2020-05-06 14:10:30 -0500

    created setup.py

---
+++ tests/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xapi-data-analyzer-HBlanco36-wboettge",
+    version="0.0.1",
+    author="Harrison White and Walt Boettge",
+    author_email="hwhite9@wisc.edu, wboettge@wisc.edu",
+    description="A Python CLI tool to clean and analyze xAPI data. Intended for use at the University of Wisconsin - "
+                "Madison using data from the DoIT xAPI Learning Locker.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HBlanco36/xapi-data-analyzer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
