e4a03d3b82c9ca62f4efed9f89e5b78309532b7e
harrison <hwhite9@wisc.edu>
2020-09-04 14:00:46 -0500

    Got going on a GUI version

--- xapi_data_analyzer/Main.py
+++ xapi_data_analyzer/Main.py
@@ -1,75 +1,78 @@
-from . import GlobalData
-from .Day import Day
+import GlobalData
+from Day import Day
 import pandas as pd
-import os
+import PySimpleGUI as sg
+import ctypes
+import platform
 
 
-def clear():
-    # Windows
-    if os.name == "nt":
-        os.system("cls")
+def make_dpi_aware():
+    """
+    Helps PySimpleGUI not be blurry on high-dpi monitors. Taken from
+    https://github.com/PySimpleGUI/PySimpleGUI/issues/1179
+    """
+    if int(platform.release()) >= 8:
+        ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
-    # Unix-based OSes
-    else:
-        os.system("clear")
+
+def create_main_window():
+    layout = [
+        [sg.Text("UW-Madison xAPI Data Analyzer", font="Any 15")],
+        [sg.Text("Please select the cleaned xAPI data .csv file from the DoIT Learning Locker "
+                 "(usually called dataMM-DD-YY(cleaned).csv)")],
+        [sg.FileBrowse(key="FILEIN")],
+        [sg.Text("Next, please enter the lower and upper bounds of the range of chapters (inclusive).")],
+        [sg.Text("Lower:"), sg.InputText(size=(1, 2), key="LOWERBOUND"), sg.Text("Upper:"),
+         sg.InputText(size=(1, 2), key="UPPERBOUND")],
+        [sg.Text("A .csv file for each chapter will be saved in the current directory, which can be imported into Excel"
+                 " for further data analysis.")],
+        [sg.Text("A .csv of the time each student spent on each chapter will also be saved to the current directory.")],
+        [sg.Text("If you have any .csv files of the same name (Day x.csv or StudentDurations.csv), THEY WILL BE "
+                 "OVERWRITTEN!", text_color="red")],
+        [sg.Button("Go", size=(1, 4)), sg.Exit()]
+    ]
+    return sg.Window("xAPI Data Analyzer", layout, element_justification="center")
 
 
 def main():
-    clear()
-    print("Welcome to the UW-Madison xAPI data analyzer!")
-    print("Authors: Walt Boettge and Harrison White\n")
+    make_dpi_aware()
+    sg.theme("SystemDefault")
 
-    view_help = input("Would you like to view instructions? [y/N] ")
-    if view_help == "y" or view_help == "Y" or view_help == "Yes" or view_help == "yes":
-        clear()
-        print("This program will generate statistics about a range of Pressbooks chapters that you select.")
-        print("You will be prompted for the lower and upper bounds of the range of chapters (inclusive). Please enter "
-              "only positive integers for this, with the lower bound less than or equal to the upper bound.\n")
-        print("The program also depends on a .csv of the xAPI data cleaned by the DoIT Learning Locker script.")
-        print("It usually has a title similar to \"dataMM-DD-YY(cleaned).csv\".")
-        print("You will be prompted to enter the path to this data file on your computer.\n")
-        print("A .csv file for each chapter will be saved in the current directory, which can be "
-              "imported into Excel for further data analysis.")
-        print("A .csv of the time each student spent on each chapter will also be saved to the current directory.\n")
-        print("IMPORTANT NOTE: if you have any .csv files of the same name (Day x.csv or StudentDurations.csv), "
-              "they WILL BE OVERWRITTEN!\n")
-    else:
-        clear()
+    main_window = create_main_window()
+    while True:
+        event, values = main_window.read()
 
-    lower_bound = None
-    upper_bound = None
-    input_incorrect = True
-    while input_incorrect:
-        lower_bound = input("Please enter the lower bound: ")
-        upper_bound = input("Please enter the upper bound: ")
-        try:
-            lower_bound = int(lower_bound)
-            upper_bound = int(upper_bound)
-            if lower_bound > upper_bound:
-                raise ValueError()
-            if lower_bound <= 0:
-                raise ValueError()
-            input_incorrect = False
-        except ValueError:
-            print("ERROR: invalid input. Please enter only positive integers, and make sure the lower bound is "
-                  "less than or equal to the upper bound.\n")
+        if event in ("Exit", None):
+            break
 
-    input_incorrect = True
-    while input_incorrect:
-        data_path = input("Please enter the path to the .csv containing the data: ")
-        try:
-            GlobalData.set_data_vars(data_path)
-            df_duration = pd.DataFrame(index=GlobalData.class_list)
+        if event == "Go":
+            try:
+                # Make sure the chapter numbers are legal
+                lower_bound = int(values["LOWERBOUND"])
+                upper_bound = int(values["UPPERBOUND"])
+                if lower_bound > upper_bound:
+                    raise ValueError()
+                if lower_bound <= 0:
+                    raise ValueError()
 
-            for i in range(int(lower_bound), int(upper_bound) + 1):
-                day = Day(i, GlobalData.raw_data, GlobalData.class_list)
-                df_duration["Chapter " + str(i)] = day.get_students_duration().values()
-                day.get_day_dataframe().to_csv("Chapter " + str(i) + ".csv")
+                GlobalData.set_data_vars(values["FILEIN"])
+                df_duration = pd.DataFrame(index=GlobalData.class_list)
 
-            df_duration.to_csv("StudentDurations.csv")
-            clear()
-            input_incorrect = False
-        except FileNotFoundError:
-            print("\nERROR: Data file not found! Please double-check the path to the data file and try again.\n")
+                for i in range(int(lower_bound), int(upper_bound) + 1):
+                    day = Day(i, GlobalData.raw_data, GlobalData.class_list)
+                    df_duration["Chapter " + str(i)] = day.get_students_duration().values()
+                    day.get_day_dataframe().to_csv("Chapter " + str(i) + ".csv")
+
+                df_duration.to_csv("StudentDurations.csv")
+
+            except ValueError:
+                sg.Popup("ERROR: invalid input. Please enter only positive integers, and make sure the lower bound is "
+                         "less than or equal to the upper bound.", title="Error")
+            except FileNotFoundError:
+                sg.Popup("ERROR: Data file not found! Please double-check the path to the data file and try again.",
+                         title="Error")
 
     print("All files successfully saved!")
+
+
+main()
