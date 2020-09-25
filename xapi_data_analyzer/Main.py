598bcb119b9a6aa5323c83359c6dc89ff121d0ea
harrison <hwhite9@wisc.edu>
2020-09-25 14:52:14 -0500

    added graphing features

--- xapi_data_analyzer/Main.py
+++ xapi_data_analyzer/Main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import PySimpleGUI as sg
 from datetime import datetime
 import pytz
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def create_main_window():
@@ -20,6 +22,19 @@ def create_main_window():
         [sg.Button("Go", size=(4, 1)), sg.Exit()]
     ]
     return sg.Window("xAPI Data Analyzer", layout, element_justification="center")
+
+
+def create_graphs_window():
+    layout = [
+        [sg.Text("Generate Graphs", font="Any 15 bold")],
+        [sg.Text("All files have been successfully saved into the current directory!")],
+        [sg.Text("If you would like, you can generate some graphs now, or use the files to make your own.")],
+        [sg.Text("Generate:")],
+        [sg.Button("Student % Interacted Graph", button_color=("white", "green"))],
+        [sg.Button("Student Count Interacted Graph", button_color=("white", "green"))],
+        [sg.Button("Student Duration Graph", button_color=("white", "green"))]
+    ]
+    return sg.Window("Generate Graphs", layout, element_justification="center")
 
 
 def main():
@@ -43,21 +58,52 @@ def main():
                 # Generate a CST timestamp + ElementCollection object
                 timestamp = datetime.now(pytz.timezone("America/Chicago"))
                 element_collection = ElementCollection(id_list, GlobalData.raw_data, GlobalData.class_list)
-                element_collection.get_dataframe().to_csv("xAPI-Data-Analyzer_" + str(timestamp) + ".csv")
 
-                df_students = pd.DataFrame.from_dict(element_collection.get_students_duration(), orient='index')
+                elements_df = element_collection.get_dataframe()
+                elements_df.to_csv("xAPI-Data-Analyzer_" + str(timestamp) + ".csv")
+
+                df_students = pd.DataFrame.from_dict(element_collection.get_students_duration(), orient='index', columns=["Durations"])
                 df_students.to_csv("StudentDurations_" + str(timestamp) + ".csv")
-
-                sg.Popup("All files successfully saved!", title="Success!")
 
             except KeyError as e:
                 sg.Popup("ERROR: The following H5P element was not found: " + str(e.args[0]), title="Error")
+                continue
             except FileNotFoundError:
                 sg.Popup("ERROR: Data file not found! Please double-check the path to the data file and try again.",
                          title="Error")
+                continue
             except ValueError:
                 sg.Popup("ERROR: The items entered in the H5P ID list were not valid integers! Please try again.",
                          title="Error")
+                continue
+
+            # Generate graphs window
+            graphs_window = create_graphs_window()
+            while True:
+                event, values = graphs_window.read()
+
+                if event is None:
+                    break
+
+                if event == "Student % Interacted Graph":
+                    elements_df.plot(x="object name?", y="% of users who interacted", kind="bar")
+                    plt.xlabel("H5P ID")
+                    plt.ylabel("Percent")
+                    plt.ylim(0, 100)
+                    plt.show()
+
+                if event == "Student Count Interacted Graph":
+                    elements_df.plot(x="object name?", y="Number of users who interacted", kind="bar")
+                    plt.xlabel("H5P ID")
+                    plt.ylim(0, len(GlobalData.class_list))
+                    plt.show()
+
+                if event == "Student Duration Graph":
+                    plt.plot(df_students["Durations"], np.zeros_like(df_students["Durations"]), "x")
+                    plt.xlabel("Duration (min)")
+                    plt.show()
+
+            graphs_window.close()
 
     main_window.close()
 
