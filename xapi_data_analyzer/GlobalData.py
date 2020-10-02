b1441f0eb835c65319fe336b7de4c8d2967f740c
harrison <hwhite9@wisc.edu>
2020-10-02 14:51:30 -0500

    convert timestamp column to datetime objects, don't call parse_dates anymore

--- xapi_data_analyzer/GlobalData.py
+++ xapi_data_analyzer/GlobalData.py
@@ -7,7 +7,7 @@ class_list = None
 def set_data_vars(data_path):
     global raw_data
     global class_list
-    raw_data = pd.read_csv(data_path, parse_dates=["Timestamp"])
+    raw_data = pd.read_csv(data_path)
 
     # Only keep around columns we care about
     raw_data = raw_data[["Email", "Verb", "object id", "Question/Slide", "Timestamp", "Duration"]]
@@ -20,6 +20,9 @@ def set_data_vars(data_path):
 
     # Reformat email column to remove the "mailto:"
     raw_data["Email"] = raw_data["Email"].str.slice(start=7)
+
+    # Convert the Timestamp column to datetime objects
+    raw_data["Timestamp"] = pd.to_datetime(raw_data["Timestamp"])
 
     # Parse the actual object ID from the "object id" column
     url_list = raw_data["object id"].to_list()
