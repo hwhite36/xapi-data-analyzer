f0dbf308307120143c355e6a2e1d361d9c9035de
harrison <hwhite9@wisc.edu>
2020-10-02 13:14:58 -0500

    fixed typo

--- xapi_data_analyzer/GlobalData.py
+++ xapi_data_analyzer/GlobalData.py
@@ -13,7 +13,7 @@ def set_data_vars(data_path):
     raw_data = raw_data[["Email", "Verb", "object id", "Question/Slide", "Timestamp", "Duration"]]
 
     # Drop data that doesn't have an email associated with it
-    raw_data = raw_data.dropbna(suset=["Email"])
+    raw_data = raw_data.dropbna(subset=["Email"])
 
     # Drop all "consumed" verbs b/c they seem to be pretty useless
     raw_data = raw_data[raw_data["Verb"] != 'consumed']
@@ -22,7 +22,5 @@ def set_data_vars(data_path):
     raw_data["Email"] = raw_data["Email"].str.slice(start=7)
 
     # TODO Parse the actual object ID from the "object id" column
-
-    # TODO check out "Duration" column, see if we even wanna keep it?
 
     class_list = set(raw_data["Email"])
