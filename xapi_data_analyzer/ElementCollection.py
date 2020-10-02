9b7569911ad2a1cf54bc6c8cebc657e74f58a719
harrison <hwhite9@wisc.edu>
2020-10-02 14:38:19 -0500

    changed old references to column names

--- xapi_data_analyzer/ElementCollection.py
+++ xapi_data_analyzer/ElementCollection.py
@@ -15,7 +15,7 @@ class ElementCollection:
 
     def __init__(self, id_list, data, class_list):
         self.id_list = id_list
-        self.data = data[data["object name?"].isin(self.id_list)]  # Now data can be trimmed
+        self.data = data[data["object id"].isin(self.id_list)]  # Now data can be trimmed
         self.class_list = class_list
         self.class_size = len(class_list)
 
@@ -29,7 +29,7 @@ class ElementCollection:
         question_name_dict = dict.fromkeys(self.id_list)
         for index, row in self.data.iterrows():
             if pd.notna(row["Question/Slide"]):
-                question_name_dict[row["object name?"]] = row["Question/Slide"]
+                question_name_dict[row["object id"]] = row["Question/Slide"]
 
         # Sorts the dictionary by key, but preserves dictionary type
         question_name_dict = collections.OrderedDict(sorted(question_name_dict.items()))
@@ -45,8 +45,8 @@ class ElementCollection:
         """
         interacted_dict = {k: [] for k in self.id_list}
         for index, row in self.data.iterrows():
-            if not (row["Name"] in interacted_dict[row["object name?"]]):
-                interacted_dict[row["object name?"]].append(row["Name"])
+            if not (row["Email"] in interacted_dict[row["object id"]]):
+                interacted_dict[row["object id"]].append(row["Email"])
 
         return interacted_dict
 
@@ -76,7 +76,7 @@ class ElementCollection:
         durations = {}
         delta_max = datetime.timedelta(minutes=10)
         for student in self.class_list:
-            student_df = self.data[self.data['Name'] == student].reset_index()
+            student_df = self.data[self.data['Email'] == student].reset_index()
             duration = datetime.timedelta()
             for index in range(0, len(student_df) - 1):
                 delta = student_df.iloc[index]['Date_Time'] - student_df.iloc[index + 1]['Date_Time']
@@ -93,7 +93,7 @@ class ElementCollection:
         :return: a complete dataframe containing all we want to know from the raw data regarding specific elements
         """
         df = pd.DataFrame(index=self.id_list)
-        df["object name?"] = self.id_list
+        df["object id"] = self.id_list
         df["Element Name"] = self.get_question_name_dict().values()
         interacted_dict_values = self.get_interacted_dict().values()
         df["List of users who interacted"] = interacted_dict_values
