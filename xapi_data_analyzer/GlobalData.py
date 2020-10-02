0d68abaf470f211a767785b50b467e8ecf97ede7
wboettge <waltb18082@isd273.org>
2020-10-02 12:23:18 -0500

    load json info into a global dict in GlobalData.py

--- xapi_data_analyzer/GlobalData.py
+++ xapi_data_analyzer/GlobalData.py
@@ -1,15 +1,29 @@
 import pandas as pd
+import json
+import PySimpleGUI as sg
 
 raw_data = None
 class_list = None
+DayInfo = None
 
 
 def set_data_vars(data_path):
     global raw_data
     global class_list
+    global DayInfo
     raw_data = pd.read_csv(data_path, parse_dates=[['Date', 'Time']])
     raw_data = raw_data.dropna(subset=["Email"])
     # We drop all "consumed" verbs here b/c they seem to be pretty useless
     raw_data = raw_data[raw_data["Verb"] != 'consumed']
 
     class_list = set(raw_data["Email"])
+
+    # import data in DayElement.json
+    try:
+        DayInfo = json.load('DayElement.json')
+
+    except json.JSONDecodeError:
+        sg.Popup("ERROR: DayElement.json could could not be read", title="Error")
+
+    except FileNotFoundError:
+        sg.Popup("ERROR: DayElement.json could could not be found", title="Error")
