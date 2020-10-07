a4c0ed90c0851d6e5e0820e0e664a9019cd69fe7
harrison <hwhite9@wisc.edu>
2020-10-07 15:00:11 -0500

    Big refactor, added ideas for how to implement graphs with the json method

--- xapi_data_analyzer/Main.py
+++ xapi_data_analyzer/Main.py
@@ -7,7 +7,12 @@ import pytz
 import matplotlib.pyplot as plt
 
 
-def create_main_window():
+def create_main_window():  # TODO add browse button for json file, add more text describing options
+    """
+    TODO fill in
+
+    :return:
+    """
     layout = [
         [sg.Text("UW-Madison xAPI Data Analyzer", font="Any 15 bold")],
         [sg.Text("Please select the xAPI data .csv file from the DoIT Learning Locker "
@@ -25,6 +30,11 @@ def create_main_window():
 
 
 def create_graphs_window():
+    """
+    TODO Fill in
+
+    :return:
+    """
     layout = [
         [sg.Text("Generate Graphs", font="Any 15 bold")],
         [sg.Text("All files have been successfully saved into the current directory!")],
@@ -38,28 +48,34 @@ def create_graphs_window():
 
 
 def use_id_list(id_list, timestamp):
-    try:
-        element_collection = ElementCollection(id_list, GlobalData.raw_data, GlobalData.class_list)
-        elements_df = element_collection.get_dataframe()
-        elements_df.to_csv("xAPI-Data-Analyzer_" + str(timestamp) + ".csv")
+    """
+    TODO Fill in
 
-        df_students = pd.DataFrame.from_dict(element_collection.get_students_duration(), orient='index')
-        df_students.to_csv("StudentDurations_" + str(timestamp) + ".csv")
+    :param id_list:
+    :param timestamp:
+    :return:
+    """
+    element_collection = ElementCollection(id_list, GlobalData.raw_data, GlobalData.class_list)
+    elements_df = element_collection.get_dataframe()
+    elements_df.to_csv("xAPI-Data-Analyzer_" + str(timestamp) + ".csv")
 
-        sg.Popup("All files successfully saved!", title="Success!")
-        return elements_df, df_students
+    df_students = pd.DataFrame.from_dict(element_collection.get_students_duration(), orient='index')
+    df_students.to_csv("StudentDurations_" + str(timestamp) + ".csv")
 
-    except KeyError as e:
-        sg.Popup("ERROR: The following H5P element was not found: " + str(e.args[0]), title="Error")
-    except FileNotFoundError:
-        sg.Popup("ERROR: Data file not found! Please double-check the path to the data file and try again.",
-                 title="Error")
-    except ValueError:
-        sg.Popup("ERROR: The items entered in the H5P ID list were not valid integers! Please try again.",
-                 title="Error")
+    sg.Popup("All files successfully saved!", title="Success!")
+
+    # Generate graphs window
+    graphs_window = create_graphs_window()
+    manage_graph_window(graphs_window, elements_df, df_students)
 
 
-def use_json(timestamp):
+def use_json(timestamp):  # TODO add prompt after each day is generated asking if the user wants to view graphs.. or auto-generate them? we could even generate the graphs (as pngs) and two csvs and put it in a day-specific folder
+    """
+    TODO fill in
+
+    :param timestamp:
+    :return:
+    """
     days = GlobalData.DayInfo['Days']
 
     for day in days.values():
@@ -74,6 +90,11 @@ def use_json(timestamp):
 
 
 def generate_timestamp():
+    """
+    TODO fill in
+
+    :return:
+    """
     # Generate a CST timestamp
     timestamp = str(datetime.now(pytz.timezone("America/Chicago")))
     # Make the string a little prettier
@@ -81,6 +102,44 @@ def generate_timestamp():
     timestamp = timestamp.replace(":", "-")
     timestamp = timestamp[:timestamp.rindex(".")]
     return timestamp
+
+
+def manage_graph_window(graphs_window, elements_df, df_students):
+    """
+    TODO fill in
+
+    :param graphs_window:
+    :param elements_df:
+    :param df_students:
+    :return:
+    """
+    while True:
+        event, values = graphs_window.read()
+
+        if event is None:
+            break
+
+        if event == "Student % Interacted Graph":
+            elements_df.plot(x="object id", y="% of users who interacted", kind="bar")
+            plt.xlabel("H5P ID")
+            plt.ylabel("Percent")
+            plt.ylim(0, 100)
+            plt.show()
+
+        if event == "Student Count Interacted Graph":
+            elements_df.plot(x="object id", y="Number of users who interacted", kind="bar")
+            plt.xlabel("H5P ID")
+            plt.ylim(0, len(GlobalData.class_list))
+            plt.show()
+
+        if event == "Student Duration Graph":
+            df_students.hist()
+            plt.xlabel("Duration (min)")
+            plt.ylabel("Number of Students")
+            plt.title("Student Durations")
+            plt.show()
+
+    graphs_window.close()
 
 
 def main():
@@ -98,6 +157,11 @@ def main():
                 GlobalData.set_data_vars(values["FILEIN"])
             except KeyError as e:
                 sg.Popup("ERROR: The following H5P element was not found: " + str(e.args[0]), title="Error")
+                continue
+            except FileNotFoundError:
+                sg.Popup("ERROR: Data file not found! Please double-check the path to the data file and try again.",
+                         title="Error")
+                continue
 
             # Parse the ID list
             id_list = values["IDLIST"]
@@ -107,41 +171,17 @@ def main():
 
             # If the user entered IDs, use those. Otherwise, use the json data
             if id_list:
-                id_list = [int(item.strip()) for item in id_list.split(",")]
-                use_id_list(id_list, timestamp)
+                try:
+                    id_list = [int(item.strip()) for item in id_list.split(",")]
+                    use_id_list(id_list, timestamp)
+                except ValueError:
+                    sg.Popup("ERROR: The items entered in the H5P ID list were not valid integers! Please try again.",
+                             title="Error")
+                    continue
             else:
                 use_json(timestamp)
 
-            # Generate graphs window
-            graphs_window = create_graphs_window()
-            while True:
-                event, values = graphs_window.read()
-
-                if event is None:
-                    break
-
-                if event == "Student % Interacted Graph":
-                    elements_df.plot(x="object id", y="% of users who interacted", kind="bar")
-                    plt.xlabel("H5P ID")
-                    plt.ylabel("Percent")
-                    plt.ylim(0, 100)
-                    plt.show()
-
-                if event == "Student Count Interacted Graph":
-                    elements_df.plot(x="object id", y="Number of users who interacted", kind="bar")
-                    plt.xlabel("H5P ID")
-                    plt.ylim(0, len(GlobalData.class_list))
-                    plt.show()
-
-                if event == "Student Duration Graph":
-                    df_students.hist()
-                    plt.xlabel("Duration (min)")
-                    plt.ylabel("Number of Students")
-                    plt.title("Student Durations")
-                    plt.show()
-
-            graphs_window.close()
-
     main_window.close()
+
 
 main()
