8f818946864df59a7bc87918770ca105455b77b4
wboettge <waltb18082@isd273.org>
2020-10-09 13:58:57 -0500

    check that data exists for the given day before running analysis and creating spreadsheets

--- xapi_data_analyzer/Main.py
+++ xapi_data_analyzer/Main.py
@@ -91,12 +91,11 @@ def use_json():
         day_ids = day['Elements']
 
         element_collection = ElementCollection(day_ids, GlobalData.raw_data, GlobalData.class_list)
-        elements_df = element_collection.get_dataframe()
-        if not elements_df.empty:
-            elements_df.to_csv("Day" + str(day_num) + "_" + str(timestamp) + ".csv")
+        # Check that the data is not empty before creating spreadsheets
+        if not element_collection.data.empty:
+            element_collection.get_dataframe().to_csv("Day" + str(day_num) + "_" + str(timestamp) + ".csv")
 
-        df_students = pd.DataFrame.from_dict(element_collection.get_students_duration(), orient='index')
-        if not df_students.empty:
+            df_students = pd.DataFrame.from_dict(element_collection.get_students_duration(), orient='index')
             df_students.to_csv("StudentDurations_Day" + str(day_num) + str(timestamp) + ".csv")
 
 def create_graphs_window():
